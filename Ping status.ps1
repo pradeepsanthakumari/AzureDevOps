49275d62de5857433ccc8b5e71149cac1b52ce25
pradeepsanthakumari <45057391+pradeepsanthakumari@users.noreply.github.com>
2018-11-15 15:32:21 +1100

    Add files via upload

---
+++ Ping status.ps1
@@ -0,0 +1,23 @@
+﻿Clear
+
+$Servers=Get-Content C:\Temp\Pradeep\Servers.txt
+
+foreach ($Server in $Servers)
+
+{
+$Status=Test-Connection -ComputerName $Server -Quiet 
+
+IF ($Status -eq "True")
+
+{
+Write-Host "$Server is UP"
+}
+
+else
+{
+Write-host "$Server is Down"
+}
+
+}
+
+Pradeep
